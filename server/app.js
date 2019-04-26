301b6254677b0de95547c0a2833b80e8d8bde2d1
kennymorgan1 <kenneth.ezeugwu@photizzo.com>
2019-04-26 13:55:24 +0100

    clone repo

--- server/app.js
+++ server/app.js
@@ -19,5 +19,6 @@ app.all('*', (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 export default app;
